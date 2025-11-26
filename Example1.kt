55b969b06a0e77c00947d177474076b5712d9359
satrirat <115156411+satrirattt@users.noreply.github.com>
2025-11-26 11:50:10 +0700

    Create Example1.kt

---
+++ Example1.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>){
+    println("Data Student : ")
+    print("=====================\n")
+    println("ID : " + args[0])
+    println("Name : " + args[1])
+    println("Age : " + args[2])
+
+}
