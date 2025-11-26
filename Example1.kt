b7c2c6902bb53a42c5569c86412e56055a2528e9
satrirat <115156411+satrirattt@users.noreply.github.com>
2025-11-26 11:52:25 +0700

    Delete Example1.kt

--- Example1.kt
+++
@@ -1,8 +0,0 @@
-fun main(args: Array<String>){
-    println("Data Student : ")
-    print("=====================\n")
-    println("ID : " + args[0])
-    println("Name : " + args[1])
-    println("Age : " + args[2])
-
-}
